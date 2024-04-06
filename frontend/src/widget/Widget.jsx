8f8763c19b0aa05fb14c3c469efa689d63935c62
Sharath chandra Raini <sharath.raini@outlook.com>
2024-04-06 13:18:47 -0500

    updated image urls

--- frontend/src/widget/Widget.jsx
+++ frontend/src/widget/Widget.jsx
@@ -5,19 +5,25 @@ import './widget.css';
 const Component1 = () => (
   <div>
     <img src="../images/GithubRepo.png" alt="Component 1" />
-    <div className="component-title">Component 1</div>
+    <div className="component-title">Github Repo</div>
   </div>
 );
 const Component2 = () => (
   <div>
-    <img src="component2.png" alt="Component 2" />
-    <div className="component-title">Component 2</div>
+    <img src="../images/MeetupGroup.png" alt="Component 2" />
+    <div className="component-title">Meet up Group</div>
   </div>
 );
 const Component3 = () => (
   <div>
-    <img src="component3.png" alt="Component 3" />
-    <div className="component-title">Component 3</div>
+    <img src="../images/RickRoll.png" alt="Component 3" />
+    <div className="component-title">Must watch React resource to become a React Guru</div>
+  </div>
+);
+const Component4 = () => (
+  <div>
+    <img src="../images/ReactResources.png" alt="Component 3" />
+    <div className="component-title">React resource</div>
   </div>
 );
 
@@ -27,6 +33,7 @@ function Widget() {
     <Component1 />,
     <Component2 />,
     <Component3 />,
+    <Component4 />
     // Add more components as needed
   ];
   const progress = (currentIndex + 1) / components.length * 100;
